96517ec60f441d5464c4afc1bdba7a1582c0a355
Daniel Machado <daniel.machado@ativagr.com.br>
2022-05-31 16:26:23 -0300

    devigenere

--- vigenere.pl
+++ vigenere.pl
@@ -8,11 +8,15 @@
 %   [f,l,a,m,e,n,g,o,' ',c,a,m,p,e,a,o]    - [6,12,1,13,5,14,7,15,0,3,1,13,16,5,1,15]
 %   [l,u,a,l,u,a,l,u,a,l,u,a,l,u,a,l,u]    - [12,21,1,12,21,1,12,21,1,12,21,1,12,21,1,12]
 %   "rGbyzosJaovnBzbA"
-     rGbyzosJaovnBzbA
+subtraiChaveEmListaDeCodigoNoTextoEmListaDeCodigo([],_,[],[]).
+subtraiChaveEmListaDeCodigoNoTextoEmListaDeCodigo([_|_],_,[],[]).
+subtraiChaveEmListaDeCodigoNoTextoEmListaDeCodigo([],ChaveCompleta,[Htexto|Ttexto],[Hresultado|Tresultado]) :-
+    subtraiChaveEmListaDeCodigoNoTextoEmListaDeCodigo(ChaveCompleta,ChaveCompleta,[Htexto|Ttexto],[Hresultado|Tresultado]).
+subtraiChaveEmListaDeCodigoNoTextoEmListaDeCodigo([Hchave|Tchave],ChaveCompleta,[Htexto|Ttexto],[Hresultado|Tresultado]) :-
+    Hresultado is mod(Htexto-Hchave,76),
+    subtraiChaveEmListaDeCodigoNoTextoEmListaDeCodigo(Tchave,ChaveCompleta,Ttexto,Tresultado).
 
 
-
-%somaChaveEmListaDeCodigoNoTextoEmListaDeCodigo(ChaveEmListaDeCodigo,TextoEmListaDeCodigo,ListaDeCodigoSomada) .
 somaChaveEmListaDeCodigoNoTextoEmListaDeCodigo([],_,[],[]).
 somaChaveEmListaDeCodigoNoTextoEmListaDeCodigo([_|_],_,[],[]).
 somaChaveEmListaDeCodigoNoTextoEmListaDeCodigo([],ChaveCompleta,[Htexto|Ttexto],[Hresultado|Tresultado]) :-
@@ -27,3 +31,7 @@ cifraVigenere(Texto, Chave, TextoCifrado) :-
     somaChaveEmListaDeCodigoNoTextoEmListaDeCodigo(ChaveEmListaDeCodigo,ChaveEmListaDeCodigo,TextoEmListaDeCodigo,Resultado),
     string2code(TextoCifrado,Resultado).
 
+decifraVigenere(Texto, Chave, TextoCifrado) :-
+    string2code(TextoCifrado,TextoEmListaDeCodigo), string2code(Chave, ChaveEmListaDeCodigo),
+    subtraiChaveEmListaDeCodigoNoTextoEmListaDeCodigo(ChaveEmListaDeCodigo,ChaveEmListaDeCodigo,TextoEmListaDeCodigo,Resultado),
+    string2code(Texto,Resultado).
